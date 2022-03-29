be2d3fff63280fcd21c6e1a46c7e38e9afb3da0c
Felipe Vieira <felipecosta1961@gmail.com>
2022-03-29 14:04:31 -0300

    This commit allows to create an account

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,26 +1,32 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+function createLoginForm() {
+	return (
+		<>
+			<Input
+				id="email"
+				type="email"
+				name="q"
+				required
+				placeholder="email"
+			></Input>
+
+			<Input id="answer" type="answer" name="a" placeholder="senha"></Input>
+			<Link to="/habitos">
+				<Button>Entrar</Button>
+			</Link>
+		</>
+	);
+}
+
 function Home() {
 	return (
 		<Container>
 			<h1>TrackIt</h1>
-			<form>
-				<input
-					id="email"
-					type="email"
-					name="q"
-					required
-					placeholder="email"
-				></input>
-
-				<input id="answer" type="answer" name="a" placeholder="senha"></input>
-				<Link to="/habitos">
-					<button>Entrar</button>
-				</Link>
-			</form>
+			<LoginForm>form</LoginForm>
 			<Link to="/cadastro">
-				<CreateAccountLink>Não tem uma conta? Cadastre-se!</CreateAccountLink>
+				<SignUpLink>Não tem uma conta? Cadastre-se!</SignUpLink>
 			</Link>
 		</Container>
 	);
@@ -42,46 +48,9 @@ const Container = styled.div`
 		font-size: 69px;
 		color: #126ba5;
 	}
-
-	form {
-		display: flex;
-		flex-direction: column;
-		width: 85%;
-		gap: 6px;
-
-		input {
-			width: 100%;
-			height: 45px;
-			border: 1px solid #d5d5d5;
-			border-radius: 5px;
-			text-indent: 5px;
-			font-family: "Lexend Deca";
-			font-style: normal;
-			font-weight: 400;
-			font-size: 18px;
-			color: #dbdbdb;
-		}
-
-		input::placeholder {
-			color: #dbdbdb;
-		}
-
-		button {
-			width: 100%;
-			height: 45px;
-			border-radius: 5px;
-			border: none;
-			color: #ffffff;
-			font-family: "Lexend Deca";
-			font-style: normal;
-			font-weight: 400;
-			font-size: 18px;
-			background-color: #52b6ff;
-		}
-	}
 `;
 
-const CreateAccountLink = styled.span`
+const SignUpLink = styled.span`
 	font-family: "Lexend Deca";
 	font-style: normal;
 	font-weight: 400;
@@ -89,4 +58,47 @@ const CreateAccountLink = styled.span`
 	text-decoration-line: underline;
 	color: #52b6ff;
 	cursor: pointer;
+`;
+
+const LoginForm = styled.form`
+	 {
+		display: flex;
+		flex-direction: column;
+		width: 85%;
+		gap: 6px;
+	}
+`;
+
+const Button = styled.button`
+	pointer-events: ${(props) => (props.load ? "none" : "auto")};
+	opacity: ${(props) => (props.load ? "0.7" : "1")};
+	width: 100%;
+	height: 45px;
+	border-radius: 5px;
+	border: none;
+	color: #ffffff;
+	font-family: "Lexend Deca";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 18px;
+	background-color: #52b6ff;
+`;
+
+const Input = styled.input`
+	background-color: ${(props) => (props.load ? "#F2F2F2;" : "#FFFFFF;")}
+	pointer-events: ${(props) => (props.load ? "none" : "auto")}
+	width: 100%;
+	height: 45px;
+	border: 1px solid #d5d5d5;
+	border-radius: 5px;
+	text-indent: 5px;
+	font-family: "Lexend Deca";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 18px;
+	color: #dbdbdb;
+	::placeholder {
+	color: #dbdbdb;
+	}
+
 `;
